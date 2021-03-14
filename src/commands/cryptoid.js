ad5851db1732d4258a8fd83444be3237bb4c0a3f
danielclough <danielclough@gmail.com>
2021-03-14 15:50:11 -0700

    update

--- src/commands/cryptoid.js
+++ src/commands/cryptoid.js
@@ -5,19 +5,19 @@ let search
 cryptoid = function(query, secondArgument) {
 	switch (query) {
 		case 'lasttxs' || 'txinfo':
-			const searchT = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&t=" + secondArgument
+			let search = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&t=" + secondArgument
 		break;
 		case 'addressfirstseen' || 'getbalance' || 'getreceivedbyaddress' || 'richrank':
-			const searchA = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&a=" + secondArgument
+			let search = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&a=" + secondArgument
 		break;
 		case 'getblockhash' || 'getblocktime':
-			const searchHeight = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&height=" + secondArgument
+			let search = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&height=" + secondArgument
 		break;
 		case 'getblockheight':
-			const searchHash = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&hash=" + secondArgument
+			let search = "https://chainz.cryptoid.info/blk/api.dws?q=" + query + "&hash=" + secondArgument
 		break;
 		default:
-			const search = "https://chainz.cryptoid.info/blk/api.dws?q=" + query
+			let search = "https://chainz.cryptoid.info/blk/api.dws?q=" + query
 
 	}
 	axios.get(search).then(resp => {
