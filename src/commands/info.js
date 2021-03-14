9e8900c2ae69f020cbf4120632a1d8b25c252246
danielclough <danielclough@gmail.com>
2021-03-14 15:16:03 -0700

    update

--- src/commands/info.js
+++ src/commands/info.js
@@ -4,24 +4,24 @@ const {getinfo} = require('../depends/blackcoin-control')
 const {getnetworkinfo} = require('../depends/blackcoin-network')
 
 async function info() {
-	let walletInfo = await getwalletinfo().catch(err => 
-		console.log('getwalletinfo', err)
-	    process.exit(0)
-	);
+	let walletInfo = await getwalletinfo().catch(err => {
+			console.log('getwalletinfo', err)
+		    process.exit(0)
+		});
 
-	let stakingInfo = await getstakinginfo().catch(err => 
-		console.log('getstakinginfo', err)
-	    process.exit(0)
-	);
+	let stakingInfo = await getstakinginfo().catch(err => {
+			console.log('getstakinginfo', err)
+		    process.exit(0)
+		});
 
-	let info = await getinfo().catch(err => 
-		console.log('getinfo', err)
-	    process.exit(0)
-	);
+	let info = await getinfo().catch(err => {
+			console.log('getinfo', err)
+		    process.exit(0)
+		});
 
-	let networkInfo = await getnetworkinfo().catch(err => 
-		console.log('getnetworkinfo', err)
-	    process.exit(0)
-	);
+	let networkInfo = await getnetworkinfo().catch(err => {
+			console.log('getnetworkinfo', err)
+		    process.exit(0)
+		});
 }
 info().catch(err => console.log(err));
