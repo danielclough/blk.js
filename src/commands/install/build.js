21767eef702ebc522d382e7d5f7f8dd1f611fe01
danielclough <danielclough@gmail.com>
2021-06-24 23:45:17 -0700

    fix dirname

--- src/commands/install/build.js
+++ src/commands/install/build.js
@@ -1,14 +1,14 @@
 const shell = require('shelljs');
 const SYSTYPE = shell.exec("echo -n $(lscpu | head -1 | tr -s ' ' | cut -d ' ' -f2)");
 
-const buildDocker = `docker build -t ${DockerHub.DockerHub}/blackcoin-more-ubase-${SYSTYPE}:${branch.branch} - --network=host < /home/${process.env.USER}/git/blk.js/src/commands/install/moreBuilder/Dockerfile.ubase-${SYSTYPE}-${branch.branch}
+const buildDocker = `docker build -t ${DockerHub.DockerHub}/blackcoin-more-ubase-${SYSTYPE}:${branch.branch} - --network=host < /home/${process.env.USER}/BlackcoinMoreBuilder/Dockerfile.ubase-${SYSTYPE}-${branch.branch}
 # ubuntu (package with full ubuntu distro)
-  docker build -t ${DockerHub.DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}:${branch.branch} - --network=host < /home/${process.env.USER}/git/blk.js/src/commands/install/moreBuilder/Dockerfile.ubuntu-${SYSTYPE}-${branch.branch}
+  docker build -t ${DockerHub.DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}:${branch.branch} - --network=host < /home/${process.env.USER}/BlackcoinMoreBuilder/Dockerfile.ubuntu-${SYSTYPE}-${branch.branch}
   docker image push ${DockerHub.DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}:${branch.branch}
 # minimal (only package binaries and scripts)
 docker run -itd  --network=host --name ubase ${DockerHub.DockerHub}/blackcoin-more-ubase-${SYSTYPE}:${branch.branch} bash
-docker cp ubase:/parts /home/${process.env.USER}/git/blk.js/src/commands/install/moreBuilder
-cd /home/${process.env.USER}/git/blk.js/src/commands/install/moreBuilder
+docker cp ubase:/parts /home/${process.env.USER}/BlackcoinMoreBuilder
+cd /home/${process.env.USER}/BlackcoinMoreBuilder
 tar -C parts -c . | docker import - ${DockerHub.DockerHub}/blackcoin-more-minimal-${SYSTYPE}:${branch.branch}
 docker image push ${DockerHub.DockerHub}/blackcoin-more-minimal-${SYSTYPE}:${branch.branch}
 docker container stop ubase`
